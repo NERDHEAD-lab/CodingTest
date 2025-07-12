e1918b9aa344249c8be1112bd046223b82d6d58f
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-07-12 09:11:07 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        assureMin0Max100(num1);
+        assureMin0Max100(num2);
+        return num1 / num2;
+    }
+
+    private void assureMin0Max100(int num) {
+        if (0 < num && num <= 100) { return; }
+        throw new IllegalArgumentException("Number must be between 0 and 100");
+    }
+}
