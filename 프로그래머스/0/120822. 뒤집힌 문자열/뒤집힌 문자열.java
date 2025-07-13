08b0ef36f43263d31e48fe54d0cc07111a278472
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-07-13 19:43:15 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.21 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String my_string) {
+        assureCondition(my_string.length());
+        
+        return new StringBuilder(my_string).reverse().toString();
+    }
+
+    private void assureCondition(int num) {
+        int MIN = 1;
+        int MAX = 1000;
+        boolean isMinInclusive = true;
+        boolean isMaxInclusive = true;
+        assureCondition(num, isMinInclusive, MIN, isMaxInclusive, MAX);
+    }
+
+    private static void assureCondition(int num, boolean isMinInclusive, int MIN, boolean isMaxInclusive, int MAX) {
+        boolean minOk = isMinInclusive ? num >= MIN : num > MIN;
+        boolean maxOk = isMaxInclusive ? num <= MAX : num < MAX;
+
+        if (!(minOk && maxOk)) {
+            throw new IllegalArgumentException(
+                    String.format("Number %d is out of range %d %s %d %s %d",
+                            num, MIN, minOk ? "<=" : "<", num, isMaxInclusive ? "<=" : "<", MAX)
+            );
+        }
+    }
+}
