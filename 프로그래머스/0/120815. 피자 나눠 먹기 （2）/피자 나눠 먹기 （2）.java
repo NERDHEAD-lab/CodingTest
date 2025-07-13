4a3fcbea5c799badb4072acafac658fb490a47ce
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-07-13 19:17:53 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(int n) {
+        assureCondition(n);
+        
+        final int PIZZA = 6; // 피자 한 판에 6조각
+        return n / gcd(n, PIZZA);
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    private void assureCondition(int num) {
+        int MIN = 1;
+        int MAX = 100;
+        boolean isMinInclusive = true;
+        boolean isMaxInclusive = true;
+        assureCondition(num, isMinInclusive, MIN, isMaxInclusive, MAX);
+    }
+
+    private static void assureCondition(int num, boolean isMinInclusive, int MIN, boolean isMaxInclusive, int MAX) {
+        boolean minOk = isMinInclusive ? num >= MIN : num > MIN;
+        boolean maxOk = isMaxInclusive ? num <= MAX : num < MAX;
+
+        if (!(minOk && maxOk)) {
+            throw new IllegalArgumentException(
+                    String.format("Number %d is out of range %d %s %d %s %d",
+                            num, MIN, minOk ? "<=" : "<", num, isMaxInclusive ? "<=" : "<", MAX)
+            );
+        }
+    }
+}
