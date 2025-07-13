609b8d4c25387dc06c1d1d2c9ff012820fdf5655
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-07-13 19:50:14 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n, int t) {
+        assureCondition(n, true, 1, true, 10);
+        assureCondition(t, true, 1, true, 15);
+
+        return n << t;
+    }
+
+    private static void assureCondition(int num, boolean isMinInclusive, int MIN, boolean isMaxInclusive, int MAX) {
+        boolean minOk = isMinInclusive ? num >= MIN : num > MIN;
+        boolean maxOk = isMaxInclusive ? num <= MAX : num < MAX;
+
+        if (!(minOk && maxOk)) {
+            throw new IllegalArgumentException(
+                    String.format("Number %d is out of range %d %s %d %s %d",
+                            num, MIN, minOk ? "<=" : "<", num, isMaxInclusive ? "<=" : "<", MAX)
+            );
+        }
+    }
+}
