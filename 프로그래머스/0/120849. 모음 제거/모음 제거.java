dc227355448786c88e95434c83252a88ecbd9132
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-07-13 20:18:15 +0900

    [level 0] Title: 모음 제거, Time: 0.16 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String my_string) {
+        final String[] exclude = {"a", "e", "i", "o", "u"};
+        return my_string.replaceAll(
+                String.join("|", exclude),
+                ""
+        );
+    }
+
+    private void assureCondition(int num) {
+        int MIN = 1;
+        int MAX = 1000;
+        boolean isMinInclusive = true;
+        boolean isMaxInclusive = true;
+        assureCondition(num, isMinInclusive, MIN, isMaxInclusive, MAX);
+    }
+
+    private static void assureCondition(int num, boolean isMinInclusive, int MIN, boolean isMaxInclusive, int MAX) {
+        boolean minOk = isMinInclusive ? num >= MIN : num > MIN;
+        boolean maxOk = isMaxInclusive ? num <= MAX : num < MAX;
+
+        if (!(minOk && maxOk)) {
+            throw new IllegalArgumentException(
+                    String.format("Number %d is out of range %d %s %d %s %d",
+                            num, MIN, minOk ? "<=" : "<", num, isMaxInclusive ? "<=" : "<", MAX)
+            );
+        }
+    }
+}
