80738ddd996845a2ec45b619f0a2a4fc9dc9254f
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-07-15 23:34:09 +0900

    [level 0] Title: 순서쌍의 개수, Time: 12.01 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,14 +1,13 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n) {
         assureCondition(n);
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                count++;
-            }
-        }
 
-        return count;
+        return (int) IntStream.rangeClosed(1, n)
+                .filter(i -> n % i == 0)
+                .count();
+
     }
 
     private static void assureCondition(int num) {
