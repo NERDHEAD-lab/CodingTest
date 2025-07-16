25c02852e16988c6379b2cf891cebbc20b7258c0
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-07-16 15:23:34 +0900

    [level 0] Title: 모스부호 (1), Time: 2.71 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,25 +1,25 @@
 import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class Solution {
-    Map<String, Character> morse = Map.ofEntries(
-            Map.entry(".-",    'a'), Map.entry("-...", 'b'), Map.entry("-.-.", 'c'),
-            Map.entry("-..",   'd'), Map.entry(".",    'e'), Map.entry("..-.", 'f'),
-            Map.entry("--.",   'g'), Map.entry("....", 'h'), Map.entry("..",   'i'),
-            Map.entry(".---",  'j'), Map.entry("-.-",  'k'), Map.entry(".-..", 'l'),
-            Map.entry("--",    'm'), Map.entry("-.",   'n'), Map.entry("---",  'o'),
-            Map.entry(".--.",  'p'), Map.entry("--.-", 'q'), Map.entry(".-.",  'r'),
-            Map.entry("...",   's'), Map.entry("-",    't'), Map.entry("..-",  'u'),
-            Map.entry("...-",  'v'), Map.entry(".--",  'w'), Map.entry("-..-", 'x'),
-            Map.entry("-.--",  'y'), Map.entry("--..", 'z')
+    Map<String, String> morse = Map.ofEntries(
+            Map.entry(".-",    "a"), Map.entry("-...", "b"), Map.entry("-.-.", "c"),
+            Map.entry("-..",   "d"), Map.entry(".",    "e"), Map.entry("..-.", "f"),
+            Map.entry("--.",   "g"), Map.entry("....", "h"), Map.entry("..",   "i"),
+            Map.entry(".---",  "j"), Map.entry("-.-",  "k"), Map.entry(".-..", "l"),
+            Map.entry("--",    "m"), Map.entry("-.",   "n"), Map.entry("---",  "o"),
+            Map.entry(".--.",  "p"), Map.entry("--.-", "q"), Map.entry(".-.",  "r"),
+            Map.entry("...",   "s"), Map.entry("-",    "t"), Map.entry("..-",  "u"),
+            Map.entry("...-",  "v"), Map.entry(".--",  "w"), Map.entry("-..-", "x"),
+            Map.entry("-.--",  "y"), Map.entry("--..", "z")
     );
 
     public String solution(String letter) {
         assureCondition(letter.length());
         return Arrays.stream(letter.split(" "))
                 .map(morse::get)
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                .toString();
+                .collect(Collectors.joining());
     }
 
     private static void assureCondition(int num) {
